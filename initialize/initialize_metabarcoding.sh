dd0e8b96da84d0836fbcb540eb8ade847589b015
Jason Selwyn <jdselwyn@gmail.com>
2025-01-10 11:26:34 -0500

    Update initialize_metabarcoding.sh

--- initialize/initialize_metabarcoding.sh
+++ initialize/initialize_metabarcoding.sh
@@ -10,13 +10,13 @@ cp ${master_dir}/scripts/run_rainbowBridge.sh ${dirname}/scripts
 cp ${master_dir}/scripts/summarise_rainbowbridge.R ${dirname}/scripts
 
 #Default YAML - need to edit as needed
-cp ${master_dir}/initial_yamls/metabarcode_rainbowbridge_paired.yml ${dirname}
+cp ${master_dir}/yamls/metabarcode_rainbowbridge_paired.yml ${dirname}
 echo "Rename and fill in file-paths for YAML found ${dirname}"
 
 #Build Folder Structure
 mkdir -p ${dirname}/data
 mkdir -p ${dirname}/output
-cp ${master_dir}/initial_readmes/metabarcoding_readme.md ${dirname}/REAME.md
+cp ${master_dir}/readmes/metabarcoding_readme.md ${dirname}/REAME.md
 
 # Instructions for next steps
 echo "Create metadata file if none exists"
